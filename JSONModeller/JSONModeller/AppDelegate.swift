22ee2a6433bf2675742d9ac977952c249772c057
Johnykutty Mathew <johnykutty@Johnykuttys-MacBook-Pro.local>
2015-05-28 19:40:23 +0530

    Project added

---
+++ JSONModeller/JSONModeller/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JSONModeller
+//
+//  Created by Johnykutty Mathew on 28/05/15.
+//  Copyright (c) 2015 Johnykutty Mathew. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JSONModeller/JSONModeller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JSONModeller
+//
+//  Created by Johnykutty Mathew on 28/05/15.
+//  Copyright (c) 2015 Johnykutty Mathew. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
